476ea7b2d15e40c17adec40fab04d4450a5fb359
nselvak <selvakumar9155@gmail.com>
2022-04-01 13:36:42 +0800

    final

--- src/main/java/vttp2022/ssf/assessment/videosearch/controllers/SearchController.java
+++ src/main/java/vttp2022/ssf/assessment/videosearch/controllers/SearchController.java
@@ -1,4 +1,37 @@
 package vttp2022.ssf.assessment.videosearch.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import vttp2022.ssf.assessment.videosearch.models.Game;
+import vttp2022.ssf.assessment.videosearch.service.SearchService;
+
+@Controller
+@RequestMapping("/search")
 public class SearchController {
+
+    @Autowired
+    private SearchService svc;
+
+    @GetMapping
+    public String search(@RequestParam(name="search", required=true) String searchName, @RequestParam(name="page_size", defaultValue = "10") Integer numberOfResult, Model model) {
+
+
+        List<Game> last = svc.search(searchName, numberOfResult);
+                        
+        if (last.isEmpty()) {
+            return "error";
+        }
+
+        model.addAttribute("list", last);
+
+        return "new";
+    }
 }
--- src/main/java/vttp2022/ssf/assessment/videosearch/service/SearchService.java
+++ src/main/java/vttp2022/ssf/assessment/videosearch/service/SearchService.java
@@ -1,5 +1,90 @@
 package vttp2022.ssf.assessment.videosearch.service;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import vttp2022.ssf.assessment.videosearch.models.Game;
+
+@Service
 public class SearchService {
+
+
+    // cannot set the environment var
+    //@Value("${rawg.api.key}")
+    //private String apiKey;
+    private String apiKey="3a568030761741a0b81ee01e82cc3d02";
+
+    public List<Game> search(String searchString, Integer count) {
+
+        List<Game> list = new ArrayList<Game>();
+
+        
+        final String URL = "https://api.rawg.io/api/games";
+        String finalURL = UriComponentsBuilder
+                    .fromUriString(URL)
+                    .queryParam("key", apiKey)
+                    .queryParam("search", searchString)
+                    .queryParam("page_size", count)
+                    .toUriString();
+
+        RequestEntity<Void> req = RequestEntity
+        .get(finalURL)
+        .accept(MediaType.APPLICATION_JSON)
+        .build();
+
+        RestTemplate template = new RestTemplate();
+
+        ResponseEntity<String> resp = null;
+
+        resp = template.exchange(req, String.class);
+
+
+        try (InputStream is = new ByteArrayInputStream(resp.getBody().getBytes())) {
+            JsonReader r = Json.createReader(is);
+            JsonObject o = r.readObject();
+            //System.out.println(">>>>> service req readobject: " + o);
+
+            JsonArray results = o.getJsonArray("results");
+            
+            // Check
+            //System.out.println(">>>>> service start arraylist: " + results);
+
+            for (int i=0; i <results.size(); i++) {
+                JsonObject temp = results.getJsonObject(i);
+                Game game = new Game();
+
+                game.setName(temp.getString("name"));
+                game.setBackgroundImage(temp.getString("background_image"));
+                game.setRating((float) temp.getInt("rating"));
+
+                list.add(game);
+            }
+            
+
+        } catch (Exception ex) {
+            System.err.printf(">>>> service Error: %s\n", ex.getMessage());
+            ex.printStackTrace();
+        }
+        
+        System.out.println(">>>>> service list: " + list);
+        return list;
+        
+    }
+
+
 
 }
